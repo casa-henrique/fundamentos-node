a9ac0ebb0e78b97c61c15fc3a7a79b24dda471d3
casa-henrique <riquecasa@gmail.com>
2022-04-25 14:53:25 -0300

    extrato bancário por data

--- src/index.js
+++ src/index.js
@@ -96,4 +96,19 @@ app.post("/withdraw", (req, res) => {
   return res.status(201).send();
 });
 
+app.get("/statement/date", (req, res) => {
+  const { customer } = req;
+  const { date } = req.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+
+  return res.json(customer.statement);
+});
+
 app.listen(3333);
